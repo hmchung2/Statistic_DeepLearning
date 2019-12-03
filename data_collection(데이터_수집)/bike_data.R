25399286c1abf71ede4f46defcf5c8b17e2d10ec
hmchung2 <hmchung2@gmail.com>
2019-12-03 09:34:15 +0900

    updating

---
+++ data_collection(데이터_수집)/bike_data.R
@@ -0,0 +1,33 @@
+
+key <- '70726a5844686d633734585747534a'
+URL <- 'http://openapi.seoul.go.kr:8088'
+
+
+'?ServiceKey=인증키&baseDt=201712&pageNo=1&numOfRows=3&type=xml'
+
+
+library(tidyverse)
+library(httr)
+library(rvest)
+library(jsonlite)
+
+res <- GET(url = URL, query = list(
+  KEY = I(key),
+  TYPE = 'json',
+  SERVICE = 'bikeList',
+  START_INDEX= 1,
+  END_INDEX=5
+  ))
+
+res
+
+
+
+Sys.setlocale("LC_CTYPE", "UTF-8")
+
+thepage = readLines('http://openapi.seoul.go.kr:8088/4255696748686d6331313976766a746c/json/bikeList/1/5/.json',encoding = "UTF-8" )
+fromJSON(thepage)
+library(jsonlite)
+jFile <- fromJSON(thepage)
+jFile$rentBikeStatus$row
+
---
+++ machine_learning(머신러닝)/K_cluster_군집분석.R
@@ -0,0 +1,48 @@
+### 군집분석
+
+#################### 데이터 확인
+library(tidyverse)
+USArrests %>% str()
+
+head(USArrests)
+
+summary(USArrests)
+cor(USArrests)
+
+USAscaled <- scale(USArrests)
+#distMat <- dist(USAscaled, method = 'euclidian', diag = T, upper= T)
+distMat <- dist(USAscaled, method = 'euclidian')
+var(USAscaled)  ################ 상관계수가 됨 스케일 된거를 분산매트릭스하면 바로 상관계수가 됨
+USAscaled %>% as.data.frame() -> temp
+
+apply(X=temp,MARGIN = 2,FUN = function(x)mean(x) %>% round(1))  ### 평균 0 인지 확인 
+
+distMat
+
+hc <- hclust(distMat, method = 'complete')
+hc
+plot(hc)
+rect.hclust(tree = hc, k = 3 , border = 'red')
+
+hclustPlot <- function(d,method,title,k){
+  hc <- hclust(d=d,method = method)
+  plot(x=hc, main = title)
+  rect.hclust(tree = hc, k = k , border = 'red')
+}
+hclustPlot(distMat,'complete','최장연결법',3)단  ########## 최장 연결법이 훨씬 괜찮다 
+
+
+hclustPlot(distMat,'ward.D2','최ㄷ연결법',3)
+
+
+#################### 비계층적 군집 분석
+#install.packages("factoextra")
+library(factoextra)
+fit1 <- eclust(x =USAscaled, FUNcluster = 'kmeans', k = 3, hc_metric = 'euclidean', seed = 1234)    ##### 주성분으로 그래프도 시각화
+print(fit1)
+fit1$silinfo$clus.avg.widths
+fit1$silinfo$widths[,3] %>%  mean()
+
+fit1$cluster
+
+
